115a8f4e7b5f16cd3d15f3e186a41f5df28d0a0f
Tam <tam.nguyen@wizeline.com>
2023-06-14 10:41:45 +0700

    remove redundant code

--- main.go
+++ main.go
@@ -1,24 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"net/http"
-
 	"github.com/rs/zerolog/log"
 	"github.com/tamnguyenwizeline/ondemand-go-bootcamp/api"
 
 	"github.com/tamnguyenwizeline/ondemand-go-bootcamp/util"
 )
-
-func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got / request\n")
-	io.WriteString(w, "This is my website!\n")
-}
-func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("got /hello request\n")
-	io.WriteString(w, "Hello, HTTP!\n")
-}
 
 func main() {
 	config, err := util.LoadConfig(".")
